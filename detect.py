60aa7f4f5ac450cd6260911f19bb068250a1582e
PHS <prasetya2423@gmail.com>
2023-03-25 03:15:31 +0700

    Update detect.py

--- detect.py
+++ detect.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 
 model = YOLO("model/yolo.pt")
-results = model.track(source="test/lalu-lintas-27260.mp4",conf=0.15 ,show=True)
+results = model.track(source="test/lalu-lintas-27260.mp4",conf=0.30 ,show=True)
